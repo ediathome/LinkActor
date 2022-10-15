ed58cf0c419b7a53dfcdc252749f32314f43fac8
ediathome <edi@ediathome.de>
2022-10-15 12:48:28 +0200

    Initial Commit

---
+++ LinkActor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LinkActor
+//
+//  Created by Martin Kolb on 15.10.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LinkActor/LinkActorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkActorApp.swift
+//  LinkActor
+//
+//  Created by Martin Kolb on 15.10.22.
+//
+
+import SwiftUI
+
+@main
+struct LinkActorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
